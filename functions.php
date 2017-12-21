bf495a65a01cc43b8afb976a51796446e41ac530
ksascomm <ksasweb@jhu.edu>
2017-12-21 15:15:50 -0500

    move menu to left, add menu to right

--- functions.php
+++ functions.php
@@ -45,4 +45,44 @@ function joints_top_nav_dropdown_depth2() {
     ));
 } /* End Top Menu */
 
-add_post_type_support( 'page', 'excerpt' );    
+add_post_type_support( 'page', 'excerpt' );
+
+//Deletes all CSS classes and id's, except for those listed in the array below
+function custom_wp_nav_menu($var) {
+  return is_array($var) ? array_intersect($var, array(
+    //List of allowed menu classes
+    'first',
+    'last',
+    'current_page_item',
+    'current_page_parent',
+    'current_page_ancestor',
+    'current-menu-ancestor',
+    'active',
+    'last'
+    )
+  ) : '';
+}
+add_filter('nav_menu_css_class', 'custom_wp_nav_menu');
+add_filter('nav_menu_item_id', 'custom_wp_nav_menu');
+add_filter('page_css_class', 'custom_wp_nav_menu');
+
+//Replaces "current-menu-item" (and similar classes) with "active"
+function current_to_active($text){
+  $replace = array(
+    //List of menu item classes that should be changed to "active"
+    'current_page_item' => 'active',
+    'current_page_parent' => 'active',
+    'current_page_ancestor' => 'active',
+    'current-menu-ancestor' => 'active'
+  );
+  $text = str_replace(array_keys($replace), $replace, $text);
+    return $text;
+}
+add_filter ('wp_nav_menu','current_to_active');
+
+//Deletes empty classes and removes the sub menu class
+function strip_empty_classes($menu) {
+  $menu = preg_replace('/ class=""| class="sub-menu"/','',$menu);
+  return $menu;
+}
+add_filter ('wp_nav_menu','strip_empty_classes');
