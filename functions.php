13ae2bcf107760c7a754364d5152f46e778c4739
ksascomm <ksasweb@jhu.edu>
2018-03-02 15:52:42 -0500

    use minified parent css

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 	$parent_style = 'site-css';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/assets/css/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/assets/css/style.min.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.min.css',
         array( $parent_style ),
