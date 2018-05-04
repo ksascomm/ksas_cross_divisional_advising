d6e626234ea4f4098d741e3f09b3e62355bd078a
ksascomm <ksasweb@jhu.edu>
2018-05-04 14:32:29 -0400

    aria-label added to featured image <header>

--- parts/featured-image.php
+++ parts/featured-image.php
@@ -2,7 +2,7 @@
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header class="featured-hero parent interior" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+	<header class="featured-hero parent interior" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]" aria-label="<?php the_title(); ?> Featured Image">
 		<div class="orbit-caption">
 			<div class="row">
 				<h1 class="entry-title featured"><?php the_title(); ?></h1>
